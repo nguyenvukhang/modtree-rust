4da1a7f8d9b5d653866f576318579eec751f5959
nguyenvukhang <brew4k@gmail.com>
2022-12-09 18:09:54 +0800

    feat: authenticate mongo-db!

--- Makefile
+++ Makefile
@@ -2,7 +2,14 @@ test:
 	cargo test --workspace
 
 build:
+	@make build-debug
+
+build-debug:
+	cargo build --workspace
+	./target/debug/database
+
+build-release:
 	cargo build --workspace --release
 	./target/release/database
 
-.PHONY: test build
+.PHONY: test build build-debug build-release
--- crates/database/src/main.rs
+++ crates/database/src/main.rs
@@ -1,11 +1,63 @@
-use mongodb::{options::ClientOptions, Client};
+use mongodb::bson::{doc, Document};
+use mongodb::options::{ClientOptions, Credential, ServerAddress};
+use mongodb::{Client, Database};
 use std::error;
 
+type BoxErr = Box<dyn error::Error>;
+
+fn some_string(s: &str) -> Option<String> {
+    Some(s.to_string())
+}
+
+async fn get_client() -> Result<Client, BoxErr> {
+    let host = ServerAddress::parse("localhost:27017")?;
+    let creds = Credential::builder()
+        .username(some_string("modtree"))
+        .password(some_string("modtree"))
+        .build();
+    let opts = ClientOptions::builder()
+        .hosts(vec![host])
+        .credential(creds)
+        .build();
+    println!("opts->{:?}", opts);
+    Ok(Client::with_options(opts)?)
+}
+
+async fn list_databases(client: &mut Client) -> Result<(), BoxErr> {
+    println!("Listing databases...");
+    for db_name in client.list_database_names(None, None).await? {
+        println!("{}", db_name);
+    }
+    Ok(())
+}
+
+async fn insert_data(db: &mut Database) -> Result<(), BoxErr> {
+    let collection = db.collection::<Document>("books");
+    let docs = vec![
+        doc! { "title": "1984", "author": "George Orwell" },
+        doc! { "title": "Animal Farm", "author": "George Orwell" },
+        doc! { "title": "The Great Gatsby", "author": "F. Scott Fitzgerald" },
+    ];
+    collection.insert_many(docs, None).await?;
+    Ok(())
+}
+
+async fn list_collections(db: &Database) -> Result<(), BoxErr> {
+    println!("Listing collections...");
+    for collection_name in db.list_collection_names(None).await? {
+        println!("{}", collection_name);
+    }
+    Ok(())
+}
+
 async fn mongo() -> Result<(), Box<dyn error::Error>> {
-    let url = "mongodb://localhost:27017";
-    let opts = ClientOptions::parse(&url).await?;
-    let client = Client::with_options(opts);
-    println!("client->{:?}", client);
+    let mut client = get_client().await.unwrap();
+    list_databases(&mut client).await.unwrap();
+    let mut db = client.database("hello");
+    db.create_collection("books", None).await.unwrap();
+    insert_data(&mut db).await?;
+    list_collections(&db).await?;
+    // List the names of the databases in that deployment.
     Ok(())
 }
 
